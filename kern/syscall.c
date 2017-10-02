653517d239be75c9d519a66f9b7e3c502d117292
Jiaye Wu <wujysh@gmail.com>
2017-10-03 00:58:36 +0800

    Exercise 3

--- kern/syscall.c
+++ kern/syscall.c
@@ -71,11 +71,27 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 	// Return any appropriate return value.
 	// LAB 1: Your code here.
 
-	panic("syscall not implemented");
+	//panic("syscall not implemented");
 
+	int ret = 0;
 	switch (syscallno) {
-	default:
-		return -E_INVAL;
+		case SYS_cputs: 
+			sys_cputs((char*)a1, a2);
+			ret = 0;
+			break;
+		case SYS_cgetc:
+			ret = sys_cgetc();
+			break;
+		case SYS_getenvid:
+			ret = sys_getenvid();
+			break;
+		case SYS_env_destroy:
+			sys_env_destroy(a1);;
+			ret = 0;
+			break;
+		default:
+			ret = -E_INVAL;
 	}
+	return ret;
 }
 
--- kern/trap.c
+++ kern/trap.c
@@ -196,8 +196,11 @@ trap_dispatch(struct Trapframe *tf)
 		return;
 	}    
 	if (tf->tf_trapno == T_SYSCALL) {
-		// LAB 1: Your code here.        
-		
+		// LAB 1: Your code here. 
+		tf->tf_regs.reg_eax = 
+			syscall(tf->tf_regs.reg_eax, tf->tf_regs.reg_edx, tf->tf_regs.reg_ecx,
+				tf->tf_regs.reg_ebx, tf->tf_regs.reg_edi, tf->tf_regs.reg_esi);
+		return;     
 	}
 	
 	// Unexpected trap: The user process or the kernel has a bug.
